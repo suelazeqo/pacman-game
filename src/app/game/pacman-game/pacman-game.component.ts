c99e0d1bc1a92d6cff35c5cfeb94ec7ce2e889c6
Suela <suela.zeqo@innovaway.al>
2023-04-06 16:17:51 +0200

    implementing playing field of pocman game with pacman and one ghost

--- src/app/game/pacman-game/pacman-game.component.ts
+++ src/app/game/pacman-game/pacman-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GameService} from "../../services/game.service";
 
 @Component({
   selector: 'app-pacman-game',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PacmanGameComponent implements OnInit {
 
-  constructor() { }
+  title = 'Pacman Game';
+  totalScore: number = 0;
+  gameMap!: Array<Array<number>>;
 
-  ngOnInit(): void {
+
+
+  // Pacman properties
+  pacmanMove: number = 2; // 1 ==> up, 2==>right, 3==>down, 4==>left
+  initPacmanX!:number;
+  initPacmanY!:number;
+
+  // Ghost Properties
+  initGhostX!:number;
+  initGhostY!:number;
+
+  constructor(private services: GameService) {
+    document.title = this.title;
+    this.gameMap = services.map;
   }
 
+  ngOnInit() {
+    let map = this.gameMap;
+    //Initial pacman coordinate (20,8)
+
+    for (let i = 0; i < map.length; i++) {
+      for (let j = 0; j < map[i]['length']; j++) {
+        if (map[i][j] === 5) {
+          this.initPacmanX = i;
+          this.initPacmanY = j;
+        }
+      }
+    }
+    for (let i = 0; i < map.length; i++) {
+      for (let j = 0; j < map[i]['length']; j++) {
+        if (map[i][j] === 3) {
+          this.initGhostX = i;
+          this.initGhostY = j;
+        }
+      }
+    }
+  }
 }
